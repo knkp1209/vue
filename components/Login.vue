081db47fbebe53a46dd0686a233ba8086a0e3551
Newdream <yangknkp@gmail.com>
2018-05-25 13:59:20 +0800

    登录页面增加 geetest 验证

--- components/Login.vue
+++ components/Login.vue
@@ -5,15 +5,20 @@
 				<h1 class="login">欢迎！</h1>
 				<el-input class="login" v-model="form.name" placeholder="账号"></el-input>
 				<el-input class="login" v-model="form.password" type="password" placeholder="密码"></el-input>
+				<div class="login box" id="captcha" ></div>
 				<el-button class="login" @click="abc" size="small" type="primary">登录</el-button>
 				<input type="text" hidden="" />
 				<el-input class="login" v-model="form.md5password" type="hidden"></el-input>
+
 			</el-form>
 		</el-row>
 	</div>
 </template>
 <script>
 import	sha1 from '../lib/sha1.js';
+import	gt from '../lib/gt.js';
+import	jq from '../lib/jquery-1.12.3.min.js';
+import	call_gt from '../lib/call_gt.js';
 export default {
 	name: 'Login',
 	data() {
@@ -54,6 +59,21 @@ export default {
 	font-size: 20px;
 }
 
+.box {
+	overflow:hidden;
+	border-radius: 2px;
+	/*box-sizing: border-box;*/
+	border-right :1px solid #ccc;
+	-moz-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+
+}
+
+.geetest_holder.geetest_wind{
+	width:260px !important;
+}
 h1 {
 	text-align: center;
 }
