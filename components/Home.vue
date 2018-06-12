b33a63d85ae9b5c684562a2073f0db2dd43f21f7
Newdream <yangknkp@gmail.com>
2018-06-12 20:08:30 +0800

    将后台返回来的权限路由列表整理并渲染视图

--- components/Home.vue
+++ components/Home.vue
@@ -2,7 +2,9 @@
   <div>
     	<el-container>
     	  <el-aside>
-          <menu-tree></menu-tree>
+          <el-menu>
+            <menu-tree :nodes="nodes"></menu-tree>
+          </el-menu>
         </el-aside>
     	  <el-main>
           <router-view/>
@@ -14,12 +16,31 @@
 import MenuTree from '@/components/MenuTree'
 export default {
   name: 'Home',
+  data () {
+    return {
+      nodes:{}
+    }
+  },
   components: { MenuTree },
-  mounted () {
-    this.$ajax({
-      method: 'get',
-      url: '/api/taobao/api/v3/taobao_goods_list?sort_type=&sort=&address=&page=1&page_num=20'
-    })
+  created () {
+    this.nodes = JSON.parse(window.sessionStorage.getItem('permission'))
+    this.Finishing()
+  },
+  methods: {
+    /*
+    由于每个组件(登录和4040的除外）都必须有菜单栏，
+    所以每个组件的都要设置父路由是home。
+    这使得本来是一级菜单且没有子菜单的变成子菜单（一级菜单有菜单的不影响）
+    下面的方法将一级菜单且没有子菜单路由重新整理成一级路由
+    (这里的路由不是真正的路由)
+    */
+    Finishing () {
+      this.nodes.forEach((item,index,temp)=>{
+        if(item.children.length <= 1){
+          temp[index] = item.children[0];
+        }
+      })
+    }
   }
 }
 
--- components/MenuTree.vue
+++ components/MenuTree.vue
@@ -1,14 +1,41 @@
 <template>
 	<div>
-		<router-link to="Main">emails</router-link>
-  		<br>
-  		<router-link to="Maina">profile</router-link>
+		<!--
+		<template v-for="(node,index) in nodes" v-if="!node.hidden">
+			<el-submenu v-if="!node.leaf">
+				a
+			</el-submenu>
+			<el-menu-item v-else>
+				b
+			</el-menu-item>
+		</template>
+		-->
+	<template v-for="(node,index) in nodes" v-if="!node.hidden">
+      <el-submenu :index="index+''" v-if="!node.leaf">
+        <template slot="title">
+          <i :class="node.iconCls"></i>{{node.name}}
+        </template>
+        <menu-tree :nodes="node.children"></menu-tree>
+      </el-submenu>
+      <template  v-if="node.leaf">
+      	<router-link :to="node.path">
+	      <el-menu-item :index="node.path">
+	        	<i :class="node.iconCls"></i>{{node.name}}
+	      </el-menu-item>
+	    </router-link>
+      </template>
+    </template>
 	</div>
 </template>
 <script>
 
 export default {
 	name: 'MenuTree',
+	props: {
+	  nodes: {
+	    required: true
+	  }
+	}
 }
 </script>
 
