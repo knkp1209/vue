51f5cf9d97c4879c2f92a4f3ebfae2ecb233eaf8
Newdream <yangknkp@gmail.com>
2018-06-12 15:54:18 +0800

    网上实现左侧菜单的代码

--- components/Home.vue
+++ components/Home.vue
@@ -2,7 +2,10 @@
   <div>
     	<el-container>
     	  <el-aside>
-          <menu-tree></menu-tree>
+          <el-menu>
+            <menu-tree :nodes="this.$router.options.routes">
+            </menu-tree>
+          </el-menu>
         </el-aside>
     	  <el-main>
           <router-view/>
@@ -14,13 +17,27 @@
 import MenuTree from '@/components/MenuTree'
 export default {
   name: 'Home',
+  data() {
+    return {
+      nodes: this.$router.options.routes
+    }
+  },
   components: { MenuTree },
-  mounted () {
-    this.$ajax({
-      method: 'get',
-      url: '/api/taobao/api/v3/taobao_goods_list?sort_type=&sort=&address=&page=1&page_num=20'
-    })
-  }
+  created() {
+    //这里没有直接使用this.$router.options.routes，是因为addRoute的路由规则，在这里this.$router.options.routes获取不到
+    //有兴趣的可以看一下源码，是为什么获取不到，但是却又有规则了 
+    //另外在开发的时候，可能由于是热部署，也会不断重复的给nodes添加元素，造成导航条有重复的，简单解决，可以设置一个开关
+    let isLoadNodes = sessionStorage.getItem('isLoadNodes')
+    if (!isLoadNodes) {
+      
+      let data = JSON.parse(window.sessionStorage.getItem('permission'))
+      console.log(data)
+      console.log(this.nodes)
+      this.nodes.push(...data)
+      console.log(this.nodes)
+      sessionStorage.setItem('isLoadNodes', 'true')
+    }
+  },
 }
 
 </script>
--- components/MenuTree.vue
+++ components/MenuTree.vue
@@ -1,14 +1,40 @@
 <template>
 	<div>
-		<router-link to="Main">emails</router-link>
-  		<br>
-  		<router-link to="Maina">profile</router-link>
+		<!--
+		<template v-for="(node,index) in nodes" v-if="!node.hidden">
+      <el-submenu :index="index+''" v-if="!node.leaf">
+        <template slot="title">
+          <i :class="node.iconCls"></i>{{node.name}}</template>
+        <menu-tree :nodes="node.children"></menu-tree>
+      </el-submenu>
+      <!-- 这里是用:route="{name:node.name}，如果不使用name跳转的话，可以在数据源那里把path写成全路径即可 -->
+      <!--
+      <el-menu-item v-if="node.leaf" :index="node.path" :route="{name:node.name}">
+        <i :class="node.iconCls"></i>{{node.name}}</el-menu-item>
+    </template>
+  -->
+    <template v-for="(node,index) in nodes">
+    	<!--
+    	<el-submenu v-if="!node.leaf">
+    		<template slot="title">{{node.name}}</template>
+
+    	</el-submenu>
+    -->
+    	<el-menu-item v-if="node.leaf">
+				
+    	</el-menu-item>
+    </template>
 	</div>
 </template>
 <script>
 
 export default {
 	name: 'MenuTree',
+	props: {
+    nodes: {
+      required: true
+    }
+  }
 }
 </script>
 
