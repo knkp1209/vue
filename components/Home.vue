e10fff944616af49cbb3347e78e95124f81d21b8
Newdream <yangknkp@gmail.com>
2018-06-13 14:06:08 +0800

    修改菜单栏样式

--- components/Home.vue
+++ components/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
       <p class="username">{{user}}</p>
+      <el-switch
+        v-model="isCollapse"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        :active-value=false
+        :inactive-value=true>
+      </el-switch>
     	<el-container>
-    	  <el-aside>
-          <el-menu>
+    	  <el-aside :style="{width : asideWitdh}">
+          <el-menu  background-color="#0f5a9f" text-color="#fff" active-text-color="#ffd04b" :collapse="isCollapse">
             <menu-tree :nodes="nodes"></menu-tree>
           </el-menu>
         </el-aside>
@@ -20,7 +27,8 @@ export default {
   data () {
     return {
       nodes:{},
-      user:''
+      user:'',
+      isCollapse: false,
     }
   },
   components: { MenuTree },
@@ -31,11 +39,11 @@ export default {
   },
   methods: {
     /*
-    由于每个组件(登录和4040的除外）都必须有菜单栏，
-    所以每个组件的都要设置父路由是home。
-    这使得本来是一级菜单且没有子菜单的变成子菜单（一级菜单有菜单的不影响）
-    下面的方法将一级菜单且没有子菜单路由重新整理成一级路由
-    (这里的路由不是真正的路由)
+    * 由于每个组件(登录和4040的除外）都必须有菜单栏，
+    * 所以每个组件的都要设置父路由是home。
+    * 这使得本来是一级菜单且没有子菜单的变成子菜单（一级菜单有菜单的不影响）
+    * 下面的方法将一级菜单且没有子菜单路由重新整理成一级路由
+    * (这里的路由不是真正的路由)
     */
     Finishing () {
       this.nodes.forEach((item,index,temp)=>{
@@ -43,6 +51,18 @@ export default {
           temp[index] = item.children[0];
         }
       })
+    },
+  },
+  computed: {
+    /*
+    * 侧边栏折叠与展开的时改 aside 的宽度
+    */
+    asideWitdh: function () {
+      if (this.isCollapse) {
+        return '64px'
+      } else {
+        return '230px'
+      }
     }
   }
 }
@@ -50,13 +70,20 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.el-aside {
-    background: #FFFAFA;
-    height: auto;
+
+.el-container {
+    position: absolute;
+    top: 60px;
+    bottom: 0px;
+    width: 100%;
 }
-.el-main {
-    background: #FFF8DC;
-    height: 100%;
+.el-aside .el-menu {
+  border-right: solid 0px;
+  height:100%
+}
+
+.el-main{
+  background: #5b7fa1;
 }
 .username {
   position: absolute;
@@ -64,5 +91,13 @@ export default {
   top: -8px;
   color:white;
   font-size:22px;
+}
+.el-switch {
+  position: absolute;
+  left: 250px;
+  top: 20px;
+}
+.collapse {
+  width:64px;
 }
 </style>
