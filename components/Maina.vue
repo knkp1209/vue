e2f39e7496ee4a562dbeff792668eeed5478f35b
Newdream <yangknkp@gmail.com>
2018-06-15 14:31:41 +0800

    版本2 用Popover 弹出框实现城市列表的显示

--- components/Maina.vue
+++ components/Maina.vue
@@ -2,15 +2,21 @@
 	<div>
 	  <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
 	  <el-dialog width="700px" title="收货地址" :visible.sync="dialogTableVisible">
-		  <template v-for="(province,i) in provinces">
-			  <p style="display: inline-block; width: 150px; padding: 20px 2px;" @mouseenter="open(i,before)">
-		      <el-checkbox :indeterminate="isIndeterminate[i]" v-model="checkAll[i]" @change="(val) => handleCheckAllChange(val,i)">{{province['province']}}</el-checkbox>
-		    </p>
-		    <el-checkbox-group size="mini"  v-model="checkList[i]" v-show="show[i]" @change="(value) => handleCheckedCitiesChange(value,i)">
-		      <el-checkbox-button v-for="(city,index) in province['city']" :label="city['name']" :key=index :checked="city['checked']">
-			  </el-checkbox-button>
-		    </el-checkbox-group>
-			</template>
+		  <p style="display: inline-block; padding: 2px 15px" v-for="(province,i) in provinces">
+		    <el-checkbox :indeterminate="isIndeterminate[i]" v-model="checkAll[i]" @change="(val) => handleCheckAllChange(val,i)"></el-checkbox>
+		    <el-popover
+				  placement="top-start"
+				  :title="province['province']"
+				  width="200"
+				  trigger="hover"
+			  >
+					<el-checkbox-group size="mini"  v-model="checkList[i]"  @change="(value) => handleCheckedCitiesChange(value,i)">
+			      <el-checkbox-button v-for="(city,index) in province['city']" :label="city['name']" :key=index :checked="city['checked']">
+				  	</el-checkbox-button>
+			    </el-checkbox-group>
+				  <el-button type="text" size="mini" slot="reference">{{province['province']}}</el-button>
+				</el-popover>
+			</p>
 	  </el-dialog>
 	</div>
 </template>
@@ -22,7 +28,6 @@ export default {
 		return {
 			provinces:'',
 			checkList: {},
-			show:{},
 			before:false,
 			isIndeterminate: {},
 			checkAll: {},
@@ -56,12 +61,10 @@ export default {
 		*/
 		creatCheckboxGroup () {
 			let temp = {}
-			let showtemp = {}
 			let isIndeterminatetemp = {}
 			let allcitytemp = {}
 			Object.keys(this.provinces).forEach((item)=> {
 				temp[item] = []
-				showtemp[item] = false
 				if (this.provinces[item]['city']) {
 					allcitytemp[item] = []
 					let count = 0;
@@ -78,20 +81,9 @@ export default {
 			})
 			this.checkList = Object.assign({}, this.checkList, temp)
 			this.checkAll = Object.assign({}, this.checkAll, this.checkAll)
-			this.show = Object.assign({}, this.show, showtemp)
 			this.isIndeterminate = Object.assign({}, this.isIndeterminate, isIndeterminatetemp)
 			this.allcity = Object.assign({}, this.allcity, allcitytemp)
 		},
-		/*
-		*	鼠标移入显示当前的，隐藏之前的
-		*/
-	    open (i,before) {
-	    	if (before){
-	      	this.show[before] = false
-	      }
-	      this.before = i;
-	    	this.show[i] = !this.show[i] 
-	    },
 	    /*
 	    *	全选
 	    */
