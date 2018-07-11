c26fcbc68c7969fda9fb19e7298bae5c2557f2f8
YangKnkp <yangknkp@email.com>
2018-07-11 16:51:36 +0800

    标签组件

---
+++ components/Tag.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <el-tag :key="index" v-for="(tag,index) in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+      {{tag}}
+    </el-tag>
+    <el-input class="input-new-tag" v-validate="'max:10'" :placeholder="tip" v-if="inputVisible" v-model="declare" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+    </el-input>
+    <el-button v-else class="button-new-tag" size="small" @click="showInput">添加</el-button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Tag',
+  data() {
+    return {
+      dynamicTags: this.storeName,
+      inputVisible: false,
+      declare: ''
+    }
+  },
+  props: {
+    storeName: { // 父组件的定义数据对象
+      required: true
+    },
+    tip: {
+
+    }
+  },
+  methods: {
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+    handleInputConfirm() {
+      let declare = this.declare;
+      if (declare) {
+        this.dynamicTags.push(declare);
+      }
+      this.inputVisible = false;
+      this.declare = '';
+    }
+  }
+}
+</script>
+<style scoped>
+.el-tag+.el-tag {
+  margin-left: 10px;
+}
+
+.button-new-tag {
+  height: 32px;
+  line-height: 30px;
+  margin-left: 10px;
+  padding-bottom: 0;
+  padding-top: 0;
+}
+
+.input-new-tag {
+  margin-left: 10px;
+  vertical-align: bottom;
+  width: 180px;
+  font-size: 12px;
+}
+</style>
