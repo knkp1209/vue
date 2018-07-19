3f756af0d87fc54af808fee51c2f054ffb06fb88
YangKnkp <yangknkp@email.com>
2018-07-19 21:43:07 +0800

    配合后端接口做调整，也优化了

--- components/Home.vue
+++ components/Home.vue
@@ -35,25 +35,8 @@ export default {
   },
   components: { MenuTree },
   created () {
-    this.nodes = JSON.parse(window.sessionStorage.getItem('permission'))
-    this.Finishing()
+    this.nodes = JSON.parse(window.sessionStorage.getItem('permission'))[0].children
     this.user = window.sessionStorage.getItem('user')
-  },
-  methods: {
-    /*
-    * 由于每个组件(登录和4040的除外）都必须有菜单栏，
-    * 所以每个组件的都要设置父路由是home。
-    * 这使得本来是一级菜单且没有子菜单的变成子菜单（一级菜单有菜单的不影响）
-    * 下面的方法将一级菜单且没有子菜单路由重新整理成一级路由
-    * (这里的路由不是真正的路由)
-    */
-    Finishing () {
-      this.nodes.forEach((item,index,temp)=>{
-        if(item.children.length <= 1){
-          temp[index] = item.children[0];
-        }
-      })
-    },
   },
   computed: {
     /*
