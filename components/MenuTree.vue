4b240ce1defdfdd32fa7633984e0e1f4113002ca
YangKnkp <yangknkp@email.com>
2018-08-22 21:58:41 +0800

    菜单和路由加入第三状态：原本状态：1 菜单项对应叶子路由（没有子路由的路由） 2 菜单树对应路由树（有子路由的）3 菜单项对应路由树 目的：使有子路由的路由可以作为菜单项,因为只有菜单项点击时会跳转

--- components/MenuTree.vue
+++ components/MenuTree.vue
@@ -1,30 +1,30 @@
 <template>
 	<div>
-		<!--
 		<template v-for="(node,index) in nodes" v-if="!node.hidden">
-			<el-submenu v-if="!node.leaf">
-				a
-			</el-submenu>
-			<el-menu-item v-else>
-				b
-			</el-menu-item>
-		</template>
-		-->
-	<template v-for="(node,index) in nodes" v-if="!node.hidden">
-      <el-submenu :index="index+''" v-if="!node.leaf">
-        <template slot="title">
-          <i :class="node.iconCls"></i>{{node.name}}
-        </template>
-        <menu-tree :nodes="node.children"></menu-tree>
-      </el-submenu>
-      <template  v-if="node.leaf">
-      	<router-link :to="node.path">
-	      <el-menu-item :index="node.path">
-	        	<i :class="node.iconCls"></i>{{node.name}}
-	      </el-menu-item>
-	    </router-link>
-      </template>
-    </template>
+		  <!-- 无跳转 -->
+	      <el-submenu :index="index+''" v-if="node.leaf  == 0">
+	        <template slot="title">
+	          <i :class="node.iconCls"></i>{{node.name}}
+	        </template>
+	        <menu-tree :nodes="node.children"></menu-tree>
+	      </el-submenu>
+	      <!-- 叶子跳转 -->
+	      <template  v-else-if="node.leaf == 1">
+	      	<router-link :to="node.path">
+		      <el-menu-item :index="node.path">
+		        	<i :class="node.iconCls"></i>{{node.name}}
+		      </el-menu-item>
+		    </router-link>
+	      </template>
+	      <!-- 树跳转 -->
+	      <template  v-else-if="node.leaf == 2">
+	      	<router-link :to="node.path">
+		      <el-menu-item :index="node.path">
+		        	<i :class="node.iconCls"></i>{{node.name}}
+		      </el-menu-item>
+		    </router-link>
+	      </template>
+	    </template>
 	</div>
 </template>
 <script>
