74730880e61b99b9751171e7d1ad1a2c8a971f2a
YangKnkp <yangknkp@email.com>
2018-09-12 12:31:30 +0800

    分类列表完成

---
+++ components/Category/Index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="Id_Home_Banner_Index">
+		<router-view />
+	</div>
+</template>
+<script>
+export default {
+}
+
+</script>
+<style>
+.Id_Home_Banner_Index .el-tabs--border-card {
+	border: 0px !important;
+}
+.Id_Home_Banner_Index .el-tabs__content {
+	display: none;
+}
+
+</style>
