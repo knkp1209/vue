987dfdede7a2e31505d3f4dcfb1822df566a1f6d
YangKnkp <yangknkp@email.com>
2019-09-27 20:56:52 +0800

    save: 调整 API 顺序及删除其他配置信息，使其只是存放接口地址

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,11 +1,17 @@
-import config from './config'
 let admin = 'admin';
 let _super = 'super'; // 加下划线避免关键字冲突
 export default {
 	GeeTest: 'geetest',
 	Login: `${admin}/login`,
 	Logout: `${admin}/logout`,
+	Fn: `${_super}/fns`, // Resource
+	Role: `${_super}/roles`, // Resource
+	Account: `${_super}/accounts`, // Resource
+	'Accounts-deleted': `${_super}/accounts-deleted`, // Get
+	'Accounts-clean': `${_super}/accounts-clean`, // Delete
+	'Provinces-Cities':`${admin}/provinces/cities`, // Get
 	Config: `${admin}/config`,
+	Upload: `${admin}/upload`,
 	Banners: `${admin}/banners`,
 	Categories: `${admin}/categories`,
 	Goods: `${admin}/goods`,
@@ -16,12 +22,4 @@ export default {
 	Team: `${admin}/productTeams`,
 	Module: `${admin}/modules`,
 	Coupon: `${admin}/coupons`,
-	Fn: `${_super}/fns`, // Resource
-	Role: `${_super}/roles`, // Resource
-	Account: `${_super}/accounts`, // Resource
-	'Accounts-deleted': `${_super}/accounts-deleted`, // Get
-	'Accounts-clean': `${_super}/accounts-clean`, // Delete
-	'Provinces-Cities':`${admin}/provinces/cities`, // Get
-	Upload: `${admin}/upload`,
-	StaticResource:config.StaticResourceUrl
 }
